15d456afa5e0512d423b5bff8a998fd8b71309fc
Mahnaz <md.jazayeri.sev@gmail.com>
2022-05-26 15:23:15 +0200

    added preloader animation

--- app.js
+++ app.js
@@ -30,3 +30,26 @@ form.addEventListener('submit', e => {
 
 });
 
+
+let loader = document.querySelector('#loader');
+
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function() {
+            loadNow(opacity - 0.04);
+        }, 50);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loader = document.getElementById('loader');
+    loadNow(1);
+});
